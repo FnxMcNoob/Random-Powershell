56ac5744e10557bff89659ef8967b9f2fbf50641
Alastair <alastair.daniel.whitham@gmail.com>
2019-05-02 05:42:30 +0100

    Adding Fibonacci script

---
+++ Get-Fibonacci.ps1
@@ -0,0 +1,40 @@
+﻿
+function Get-Fibonacci{
+  [cmdletbinding()]
+  Param (
+    [parameter()]
+    [int]$Num,
+    [parameter()]
+    [switch]$ZeroStart
+  )
+
+  if ($PSBoundParameters.ContainsKey('ZeroStart')) {
+    $Result = 0
+    $J = 2
+    $Val1 = 0
+    $Val2 = 1
+    $Total = $Num
+  }
+  else {
+    $Result = 1
+    $J = 1
+    $Val1 = 1
+    $Val2 = 0
+    $Total = $Num - 1
+  }
+
+  Write-Output $Result
+
+  ## Loops through each value till $Num value is reached
+  for ($i = $J; $i -le $Total; $i++) {
+    $Result = $Val1 + $Val2
+    if ($Result -eq 0) {
+      $Result = 1
+    }
+    Write-Output $Result
+    $Val2 = $Val1
+    $Val1 = $Result
+  }
+}
+## Example
+#Get-Fibonacci(5)
